53ad700b41a41eef1b6c5dbd8297d288dcdaf949
Lucas Guedes <lucasguedes2908@gmail.com>
2024-09-09 20:55:01 -0300

    Adicionando rota de login, adicinando biblioteca de criptografia e adicionando usuário padrão ao banco de dados

--- back-end/controllers/usuarios.controllers.js
+++ back-end/controllers/usuarios.controllers.js
@@ -1,12 +1,24 @@
-//Inciar o banco de dados e chamar a função assincrona aqui
-//const { login } = require('../')
+const bcrypt = require('../utils/bcrypt.ultil');
+const pool = require('../database/user.postress'); // O arquivo onde configurou a conexão
 
 async function loginUser(req, res, next){
     try {
-        console.log("Login teste", req.body)
-        //const login = await login(req.body)
-        res.status(200).json('Sucesso');
-        //next()
+        const email = req.body.usuario.email
+        const user = await pool.query(`SELECT * FROM usuario WHERE email = '${email}'`);
+        console.log(user.rows)
+
+        if(!user){
+            console.error(`Usuário ou senha incorretos`);
+        }
+        console.log(req.body.usuario.senha, user.rows[0].senha)
+        const validar_senha = bcrypt.compare(req.body.usuario.senha, user.rows[0].senha);
+        if(validar_senha){
+            res.status(200).json('Usuário logado com sucesso');
+            next()
+        }
+        else{
+            console.error(`Usuário ou senha incorretos`);
+        }
     } catch (err) {
         console.log(err)
         console.error(`Erro ao realizar login do paciente`);
---
+++ back-end/database/user.postress.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+// Configurar os dados de conexão ao banco
+const pool = new Pool({
+  user: 'root',
+  host: 'localhost',
+  database: 'dayOne',
+  password: 'password',
+  port: 5432, // Porta padrão do PostgreSQL
+});
+
+
+module.exports = pool;
