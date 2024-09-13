2bba8c3879c796a0d4f61a7f602629857ac9a4fe
João Paulo <joaopaulosv068@gmail.com>
2024-09-13 19:44:58 -0300

    Tela de Cadastro

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import CadastrarView from '@/views/CadastrarView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/cadastro',
+      name: 'cadastro',
+      component: CadastrarView
     },
     {
       path: '/dashbord',
---
+++ front-end/src/views/CadastrarView.vue
@@ -0,0 +1,208 @@
+<template>
+    <div class="container_cadastro">
+        <div class="esquerda_cadastro"></div>
+        <div class="direita_cadastro">
+            <h1><span class="day">Day</span><span class="one">One</span></h1>
+            <form class="cadastro_form">
+                <h2>Realizar Cadastro</h2>
+                <label for="nome_completo">Nome Completo</label>
+                <input type="text" name="nome_completo" placeholder="Digite o seu nome completo">
+                <label for="dependencia">Tipo de Dependência</label>
+                <select name="dependencia" id="dependencia">
+                    <option value="alcool">Álcool</option>
+                    <option value="drogas">Drogas</option>
+                    <option value="jogos">Jogos</option>
+                    <option value="tecnologica">Tecnologica</option>
+                    <option value="emocional">Emocional</option>
+                </select>
+                <label for="email">E-mail</label>
+                <input type="text" name="email" placeholder="Digite o seu e-mail">
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" placeholder="Digite a sua senha">
+                <button type="submit" class="btn_cadastrar">Cadastar-se</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style>
+body {
+    margin: 0;
+    font-family: 'Montserrat', sans-serif;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    background-color: #110033;
+}
+
+
+.container_cadastro {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+h1 {
+    font-size: 70px;
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 15px;
+}
+
+
+.day {
+    color: #E0E1DD; 
+}
+
+
+.one {
+    color: #9d4edd; 
+}
+
+
+h2 {
+    color: #E0E1DD;
+    font-size: 25px;
+}
+
+
+.esquerda_cadastro {
+    width: 50vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url(../assets/imagemoneday.png);
+    background-size: cover;
+    background-position: center;
+}
+
+
+.direita_cadastro {
+    width: 50vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+
+.cadastro_form {
+    width: 80%;
+}
+
+.cadastro_form label {
+    display: block;
+    margin-bottom: 15px;
+    color: #E0E1DD;
+}
+
+
+.cadastro_form input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    background-color: #110033;
+    border: 1px solid #5a189a;
+    border-radius: 4px;
+    box-sizing: border-box;
+    color: #E0E1DD;
+    font-family: 'Montserrat', sans-serif;
+}
+
+.cadastro_form input::placeholder {
+    color: #D9D9D9;
+}
+
+.cadastro_form button {
+    width: 100%; 
+    padding: 10px; 
+    margin-bottom: 15px; 
+    border-radius: 4px;
+    border: none;
+    color: #ffffff;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 14px;
+    cursor: pointer;
+    box-sizing: border-box;
+}
+
+
+.btn_cadastrar {
+    background-color: #3c096c;
+    margin-top: 15px;
+}
+
+.cadastro_form p.cadastro_info {
+    color: #E0E1DD;
+    font-size: 14px;
+    margin-top: 20px;
+    text-align: center;
+    font-size: 15px;
+}
+
+.cadastro_form a {
+    color: #E0E1DD;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.cadastro_form select {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    background-color: #110033;
+    border: 1px solid #5a189a;
+    border-radius: 4px;
+    color: #E0E1DD;
+    font-family: 'Montserrat', sans-serif;
+}
+
+@media (max-width: 768px) {
+    .container_cadastro {
+        flex-direction: column; 
+    }
+
+
+    .esquerda_cadastro, .direita_cadastro {
+        width: 100%;
+        height: 50vh; 
+    }
+
+
+    h1 {
+        font-size: 50px;
+    }
+
+
+    .cadastro_form h2 {
+        font-size: 20px;
+    }
+
+
+    .cadastro_form {
+        padding: 10px;
+    }
+}
+</style>
+
+<script>
+import router from '@/router';
+import Swal from 'sweetalert2'
+import axios from 'axios';
+
+export default {
+    name:'CadastrarView',
+    data(){
+        return{
+            email:'',
+            senha:'',
+        }
+    },
+}
+</script>
