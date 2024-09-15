e4e5b9441cf74ee7a424716330036319f084fa06
João Paulo <joaopaulosv068@gmail.com>
2024-09-14 22:56:57 -0300

    Tela de Podcasts

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -31,6 +31,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/RegistrarProgressoView.vue')
     },
+    {
+      path: '/podcasts',
+      name: 'podcasts',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PodcastsView.vue')
+    },
   ]
 })
 
---
+++ front-end/src/views/PodcastsView.vue
@@ -0,0 +1,73 @@
+<template>
+    <Navbar  />
+    <main class="container_podcasts">
+        <h1>Podcasts:</h1>
+        <section class="podcast-list">
+            <div class="podcast-item">
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/2ZDBtiG0ur760iZpxJfGvR?utm_source=generator" width="100%" height="152" frameborder="0" allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>            
+            </div> 
+            <div class="podcast-item">
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1KmR6uCTXDmzct5MXlyYI8?utm_source=generator" width="100%" height="152" frameborder="0" allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>            
+            </div>
+            <div class="podcast-item">
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/0JWC4fZsrYMXkKRYR1IhzJ?utm_source=generator" width="100%" height="152" frameborder="0" allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>            
+            </div>
+            <div class="podcast-item">
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1whVvdo5lLaG4e6h1DTIQK?utm_source=generator" width="100%" height="152" frameborder="0" allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>        
+        </section>
+    </main>
+</template>
+
+<style>
+body {
+    background-color: #110033;
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+.container_podcasts {
+    width: 90%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 20px;
+}
+
+h1 {
+    color: white;
+}
+
+.podcast-list {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    margin-top: 20px;
+}
+
+.podcast-item {
+    background-color: #240046;
+    border: 1px solid #5a189a;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    text-align: center;
+}
+
+.podcast-item h2 {
+    margin-bottom: 15px;
+    font-size: 1.5rem;
+}
+
+</style>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+    name: 'podcasts',
+    components: {
+        Navbar
+    },
+}
+</script>
